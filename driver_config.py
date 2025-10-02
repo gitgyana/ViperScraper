1cbce23005d6a7dc2e1349c802c3519a79e49e62
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-02 17:16:11 +0530

    Implement selenium libs for automate

--- driver_config.py
+++ driver_config.py
@@ -6,6 +6,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def detect_os_arch():
