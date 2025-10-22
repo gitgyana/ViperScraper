c2301ea59d67e11dcfc5684304b9878af1844147
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-23 02:27:34 +0530

    Setup viper scraper services

---
+++ setup_services.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SOURCE_DIR="./systemd_services"
+DEST_DIR="/etc/systemd/system"
+
+echo "Moving .service and .timer files to $DEST_DIR..."
+sudo mv $SOURCE_DIR/*.service $DEST_DIR/
+sudo mv $SOURCE_DIR/*.timer $DEST_DIR/
+
+echo "Reloading systemd daemon..."
+sudo systemctl daemon-reload
+
+echo "Enabling and starting viper_scraper.service..."
+sudo systemctl enable viper_scraper.service
+sudo systemctl start viper_scraper.service
+
+echo "Enabling and starting viper_scraper_start.timer..."
+sudo systemctl enable viper_scraper_start.timer
+sudo systemctl start viper_scraper_start.timer
+
+echo "Enabling and starting viper_scraper_stop.timer..."
+sudo systemctl enable viper_scraper_stop.timer
+sudo systemctl start viper_scraper_stop.timer
+
+echo "Displaying logs for viper_scraper.service..."
+journalctl -u viper_scraper.service
