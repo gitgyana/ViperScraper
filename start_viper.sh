a7cedeb19d027572ac4a28c4f0512ddefe909eba
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-23 03:12:37 +0530

    Log start viper

--- start_viper.sh
+++ start_viper.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Starting Viper Scraper" >> /home/debian/Projects/ViperScraper/start_viper.log
+echo "Activating virtual environment..." >> /home/debian/Projects/ViperScraper/start_viper.log
 source /home/debian/Projects/ViperScraper/scraperEnv/bin/activate
-echo "Virtual Environment Activated"
-python /home/debian/Projects/ViperScraper/viper_scraper.py
+echo "Virtual environment activated" >> /home/debian/Projects/ViperScraper/start_viper.log
+python /home/debian/Projects/ViperScraper/viper_scraper.py >> /home/debian/Projects/ViperScraper/start_viper.log 2>&1
+echo "Viper scraper script finished" >> /home/debian/Projects/ViperScraper/start_viper.log
