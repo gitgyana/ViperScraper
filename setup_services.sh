cd2d7597cb74ebe184446af4357159162db21109
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-24 03:40:59 +0530

    Refine setup script

--- setup_services.sh
+++ setup_services.sh
@@ -7,14 +7,13 @@ echo "Copy .service and .timer files to $DEST_DIR..."
 sudo cp $SOURCE_DIR/*.service $DEST_DIR/
 sudo cp $SOURCE_DIR/*.timer $DEST_DIR/
 
-echo "Make the start_viper.sh script executable"
-chmod +x start_viper.sh
+echo "Make the start_viper.sh script executable..."
+chmod +x /home/debian/Projects/ViperScraper/start_viper.sh
 
-echo "Make script readable and executable by everyone"
-sudo chmod 755 start_viper.sh
+echo "Make script readable and executable by everyone..."
+sudo chmod 755 /home/debian/Projects/ViperScraper/start_viper.sh
 
-echo "Stop and disable existing viper services and timers"
-
+echo "Stop and disable existing viper services and timers..."
 sudo systemctl stop viper_scraper.service
 sudo systemctl disable viper_scraper.service
 
@@ -26,7 +25,6 @@ sudo systemctl disable viper_scraper_stop.timer
 
 echo "Reloading systemd daemon..."
 sudo systemctl daemon-reload
-sudo systemctl restart viper_scraper.service
 
 echo "Enabling and starting viper_scraper.service..."
 sudo systemctl enable viper_scraper.service
