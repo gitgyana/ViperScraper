1d78bc715b42769607672078d8ed9b6c6b2dabce
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-25 06:31:46 +0530

    Remove sudo inside; run it as sudo

--- reload_services.sh
+++ reload_services.sh
@@ -4,12 +4,12 @@ echo "Reloading systemd daemon..."
 sudo systemctl daemon-reload
 
 echo "Enabling and starting viper_scraper.service..."
-sudo systemctl enable viper_scraper.service
-sudo systemctl start viper_scraper.service
+systemctl enable viper_scraper.service
+systemctl start viper_scraper.service
 
 echo "Enabling and starting viper_scraper_start.timer..."
-sudo systemctl enable viper_scraper_start.timer
-sudo systemctl start viper_scraper_start.timer
+systemctl enable viper_scraper_start.timer
+systemctl start viper_scraper_start.timer
 
 echo "Displaying logs for viper_scraper.service..."
 journalctl -u viper_scraper.service
--- remove_services.sh
+++ remove_services.sh
@@ -2,11 +2,11 @@
 
 echo "Stop and disable existing viper services and timers"
 
-sudo systemctl stop viper_scraper.service
-sudo systemctl disable viper_scraper.service
+systemctl stop viper_scraper.service
+systemctl disable viper_scraper.service
 
-sudo systemctl stop viper_scraper_start.timer
-sudo systemctl disable viper_scraper_start.timer
+systemctl stop viper_scraper_start.timer
+systemctl disable viper_scraper_start.timer
 
-sudo systemctl status viper_scraper.service
-sudo systemctl status viper_scraper_start.timer
+systemctl status viper_scraper.service
+systemctl status viper_scraper_start.timer
