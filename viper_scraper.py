21196e87c7cc84ed491ba13be7f091e8f13ab83c
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-25 06:30:22 +0530

    Oneshot driver lifetime

--- viper_scraper.py
+++ viper_scraper.py
@@ -15,13 +15,14 @@ def main():
     Executes the primary workflow of the program.
     """
     try:
-        for sl in range(0, len(urls)):
-            driver = config.create_driver()
-            wait = WebDriverWait(driver, 10)
-
-            scraper = ForumScraper(driver, wait)
-
+        driver = config.create_driver()
+        wait = WebDriverWait(driver, 10)
+        scraper = ForumScraper(driver, wait)
+    except Exception as e:
+        log("error", f"Error in main driver creation: {e}")
             
+    try:
+        for sl in range(0, len(urls)):
             log("info", f"URL[{sl} / {len(urls) - 1}]: {urls[sl]}")
             scraper.scrape_all_pages(base_url=urls[sl], page_count=10)
             
@@ -30,11 +31,15 @@ def main():
 
             scraper.forum_data = []
             
-            driver.quit()
-            time.sleep(10)
-
     except Exception as e:
         log("error", f"Error in main: {e}")
+    finally:
+        try:
+            driver.quit()
+        except Exception as last_err:
+            log("error", f"Error in main driver quit: {e}")
+            
+        time.sleep(10)
 
 
 if __name__ == '__main__':
